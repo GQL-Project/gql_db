f3536071caa15a099724b442a87ae3d9335a8bc8
A-Wadhwani <wadhwani@purdue.edu>
2022-09-17 00:02:33 -0400

    [User Story 1] Created CLI for Server

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
 use server::connection::Connection;
 use server::server::db_connection::database_connection_server::DatabaseConnectionServer;
 use tonic::transport::Server;
+use clap::Parser;
 
 mod server;
 mod util;
 mod parser;
 
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = "None")]
+struct Args {
+    // IP Address 
+    #[clap(short, long, default_value = "[::1]")]
+    ip: String,
+
+    /// Port Number
+    #[clap(short, long, default_value = "50051")]
+    port: u16,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse().unwrap();
+    let args = Args::parse();
+    let addr = format!("{}:{}", args.ip, args.port).parse().unwrap();
     let db_service = Connection::default();
 
+    println!("GQL Server Started on address: {}", addr);
     Server::builder()
         .add_service(DatabaseConnectionServer::new(db_service))
         .serve_with_shutdown(addr, async {
