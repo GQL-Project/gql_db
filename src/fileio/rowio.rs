67ee9d24041c306b146d651c6ec855d3c8b83e51
A-Wadhwani <wadhwani@purdue.edu>
2022-09-18 15:50:36 -0400

    [User Story 3] Implemented Row Reads

--- src/fileio/rowio.rs
+++ src/fileio/rowio.rs
@@ -4,18 +4,19 @@ use super::{header::*, pageio::*};
 
 pub type Row = Vec<ValueType>;
 
-pub fn read_row(schema: &Schema, page: &Page, rownum: u16) -> Resut<Row, String> {
+pub fn read_row(schema: &Schema, page: &Page, rownum: u16) -> Result<Option<Row>, String> {
     let mut row = Row::new();
     let size = schema_size(schema) + 1;
     let mut offset = (rownum as usize) * size + 1;
     let check: u8 = read_type(page, offset)?;
     if check == 0 {
-        return None;
+        return Ok(None);
     }
     for (_, celltype) in schema {
         row.push(celltype.read(page, offset)?);
+        offset += celltype.size();
     }
-    Some(row)
+    Ok(Some(row))
 }
 
 pub fn write_row(schema: &Schema, page: &Page, row: Row, rownum: u16) {
