e9a0836c8920b6ff7c645a2bc2338701af844d80
juneseo27 <seo83@purdue.edu>
2022-09-20 09:45:54 -0400

    Added >GQL, multiple lines
    It reads the command that the client sends over. Just needs to test if request actually works.

--- src/client/main.rs
+++ src/client/main.rs
@@ -1,6 +1,7 @@
 use connection::database_connection_client::DatabaseConnectionClient;
 use connection::{ConnectResult, QueryRequest};
 use tonic::Request;
+use std::io::{self, BufRead, Write};
 
 pub mod connection {
     tonic::include_proto!("db_connection");
@@ -12,12 +13,44 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let request = tonic::Request::new(());
     let response = client.connect_db(request).await?.into_inner();
 
-    println!("{:?}", &response.id);
+    print!("{}", "GQL> ");
+    io::stdout().flush(); // Yellow line under is a little sketch, but it works
+
+    let mut lines = io::stdin().lock().lines();
+    let mut command = String::new();
+
+    while let Some(line) = lines.next() {
+        let last_input = line.unwrap();
+
+        // add a new line once user_input starts storing user input
+        if command.len() > 0 {
+            command.push_str("\n");
+        }
+
+        // store user input
+        command.push_str(&last_input);
+
+        // stop reading if there's a semi colon
+        if last_input.contains(";") {
+            break;
+        }
+
+        print!("{}", ">");
+        io::stdout().flush();
+    }
+
+    command = command.replace(";", "");
+    command = command.replace("\n", " ");
+
+    println!("This is the command {}", command);
+
     let request = QueryRequest {
         id: String::from(response.id.clone()),
-        query: String::from("select * from tables"),
+        query: String::from(command),
     };
-    
+
+
+
     let get_response = client.run_query(Request::new(request.clone())).await?;
     println!("{:?}", get_response.into_inner().column_names);
 
