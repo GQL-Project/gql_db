f2b27ecdb5862d16a18a1d735f9c082dcc157d93
juneseo27 <seo83@purdue.edu>
2022-09-22 19:35:11 -0400

    infinite loop for commands, VC commands parsing started

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -9,9 +9,16 @@ pub fn parse(query: &str, update: bool) -> Result<String, String> {
 
 
 /// This method parses a version control command's query string into the individual components.
+/// Format "GQL <command> <flags> <args>"
 pub fn parse_vc_cmd(query: &str) -> Result<String, String> {
-    if query.len() == 0 {
+    if query.len() == 0 || query == "GQL" {
         return Err("Empty VC Command query".to_string());
     }
+    let command_arr = query.split(" ");
+    // testing
+    for db in command_arr {
+        println!("{:?}", db);
+    }
+
     Ok("1".to_string())
 }
