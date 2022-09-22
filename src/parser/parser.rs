648f73b05e8d6c67d92c6b72dc30087bae0226a2
Shayne Marques <marques0@purdue.edu>
2022-09-22 17:30:03 -0400

    [US2] Added SQL parser implementation

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,12 +1,20 @@
+use sqlparser::dialect::GenericDialect;
+use sqlparser::parser::Parser;
+
 /// A parse function, that starts with a string and returns an AST representation of the query.
 /// If an error happens, an Err(msg) is returned.
 pub fn parse(query: &str, update: bool) -> Result<String, String> {
     if query.len() == 0 {
         return Err("Empty query".to_string());
     }
+
+    let dialect = GenericDialect {};
+
+    let ast = Parser::parse_sql(&dialect, query);
+
+    println!("AST: {:?}", ast);
     Ok(query.to_string())
 }
-
 
 /// This method parses a version control command's query string into the individual components.
 pub fn parse_vc_cmd(query: &str) -> Result<String, String> {
