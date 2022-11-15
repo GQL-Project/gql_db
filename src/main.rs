ca588ea6c0f580c3b18a6530c0e6accc8175797a
A-Wadhwani <wadhwani@purdue.edu>
2022-11-15 12:15:22 -0500

    Benchmark Mode in CLI tool

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,10 @@ struct Args {
     /// Use Demo Database
     #[clap(short, long)]
     demo: bool,
+
+    // Use benchmark Database with specified number of rows
+    #[clap(short, long)]
+    bench: Option<usize>,
 }
 
 #[tokio::main]
@@ -44,8 +48,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let addr = format!("{}:{}", args.ip, args.port).parse().unwrap();
         let db_service = Connection::default();
         println!("GQL Server Started on address: {}", addr);
-        if true {
+        if args.demo {
             bench::create_demo_db("demo");
+        } else if let Some(num_rows) = args.bench {
+            bench::create_huge_bench_db(num_rows, true);
         }
         Server::builder()
             .add_service(DatabaseConnectionServer::new(db_service))
